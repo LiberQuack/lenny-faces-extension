b571bb0f55e58424f3da5a124f7110602c30b854
ThiagoMartins <rogue.thiago@gmail.com>
2016-02-18 02:01:46 -0200

    Improved build task for transpiling es6 and zipping

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,29 @@
 const gulp = require('gulp');
+const copy = require('gulp-copy');
+const babel = require("gulp-babel");
 const zip = require('gulp-zip');
+const sequencer = require('gulp-run-sequence');
 
-gulp.task('default', function() {
-    return gulp.src(['*','!node_modules','!.idea',''])
+
+var sourceFiles = ['src/**/*','bower_components/**/*','manifest.json','!node_modules','!.idea','!.git'];
+
+gulp.task('copy', function() {
+  return gulp.src(sourceFiles)
+    .pipe(copy('.tmp'));
+});
+
+gulp.task('es6', function() {
+  return gulp.src('.tmp/src/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('.tmp/src'));
+});
+
+gulp.task('zip', function() {
+  return gulp.src('.tmp/**/*')
         .pipe(zip('lenny-faces.zip'))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', function() {
+  sequencer('copy','es6','zip');
 });
